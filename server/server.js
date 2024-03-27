bd7b5f0a6164e668c24ea2355ae54075ad59c921
ike imbanu <imbanu1@gmail.com>
2024-03-27 17:37:26 -0300

    content added to server.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
