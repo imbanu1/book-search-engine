7529c712e6a0625913b6ed5b7c168700f9f4282c
ike imbanu <imbanu1@gmail.com>
2024-03-29 15:17:49 -0300

    added to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,16 +2,34 @@ const { User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 
+const resolvers = {
 Query: {
     me: async (parent, args, context) => {
         if (context.user) {
-           const userData = await User.findOne({ _id: context.user._id.select("-__v -password");
-        return data;
+           const userData = await User.findOne({ _id: context.user._id.select("-__v -password").populate("book");
+        return userData;
         }
         throw new AuthenticationError("You must be signed in");
         },
-        users: async ( => {
+        users: async () => {
             return User.find().select("-__v -password").populate("book");
-        })
+        },
+},
+},
+
+Mutation: {
+login: async (parent, { email, password }) => {
+    const user = await User.create({ username, email, password});
+    if (!user) {
+        throw new AuthenticationError("Invalid password");
     }
+    const  correctPw = await.isCorrectPassword(password);
+    if (!correctPw) {
+        throw new AuthenticationError("Invalid password");
+    }
+    const token = signToken(user);
+    return {token, user};
+},
+
+
 }
