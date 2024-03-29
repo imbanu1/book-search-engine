5a6336fe39362926e667671aac192442a483c6f5
ike imbanu <imbanu1@gmail.com>
2024-03-29 18:19:14 -0300

    added to typedefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,8 +26,10 @@ type User {
         me: User
     }
     type Mutation {
-        login(email: String!, password: String!) Auth
-        saveBook (): User
-        removeBook(): User
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email:String!, password: String!): Auth
+        saveBook(bookInput: BookInput): User
+        removeBook(bookId: ID!): User
     }
-}
+`;
+module.exports = typeDefs;
