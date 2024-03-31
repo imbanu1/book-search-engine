9160c1fbac617a8553216e372886528efa41e814
ike imbanu <imbanu1@gmail.com>
2024-03-31 17:50:10 -0300

    added to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-impport { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./SavedBooks";
 
 import './App.css';
 import { Outlet } from 'react-router-dom';
