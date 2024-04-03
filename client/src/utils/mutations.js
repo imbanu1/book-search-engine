b571b227b916656b9ee29f3978de1a7635349176
ike imbanu <imbanu1@gmail.com>
2024-04-03 20:08:52 -0300

    created mutation.js files

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -10,4 +10,23 @@ mutation login($email: String!, $password: String!) {
             username
         }
     }
-};
+}
+`;
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String! $password: String!) {
+    addUser(username:$username, email: $email, password: $password) {
+        token,
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($book: SavedBookInput!) {
+    saveBook(book: $book) {
+        username
+    }
+}
