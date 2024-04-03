d7d5b287742a243234705eb4500ce2fcbd36d7af
ike imbanu <imbanu1@gmail.com>
2024-04-03 20:17:50 -0300

    added  mutation.js files

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -42,3 +42,21 @@ mutation saveBook($book: SavedBookInput!) {
 }
 `;
 
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}
+`;
