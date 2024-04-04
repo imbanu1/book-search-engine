1a3cf510350c2ba2206c4e2a8e6959faac74e04f
ike imbanu <imbanu1@gmail.com>
2024-04-04 13:55:37 -0300

    made changes

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const path = require('path');
+const { typeDefs, resolvers } = require('./server/schemas');
+const { authMiddleware } = require('./server/utils/auth');
+const db = require('./server/config/connection');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+async function startApolloServer() {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
+await server.start();
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
@@ -31,5 +34,6 @@ app.get("*", (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${POST}${server.graphqPath} `)
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqPath} `)
 });
+}
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,29 +1,41 @@
 const { gql } =require("apollo-server-express");
 
 const typeDefs = gql`
+input BookInput {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
 type User {
     _id: ID
-    type User {
-        _id: ID
         username: String
         email: String
-        bookCount: int
-        SavedBooks: [Book]
+        bookCount: Int
+        savedBooks: [Book]
     }
+  
     type Book {
         bookId: ID
-        authors: [string]
+        authors: [String]
         description: String
         title: String
         image: String
         link: String
     }
+
     type Auth {
         token: ID
         user: User
     }
+
     type Query {
         me: User
+        users: [User]
+        user(username: String!): User
     }
     type Mutation {
         login(email: String!, password: String!): Auth
