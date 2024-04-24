42fb774522614b4fef8b295e0bfe9a8a789a6076
ike imbanu <imbanu1@gmail.com>
2024-04-24 01:31:04 -0300

    Changes made

---
+++ server/config/seeds.js
@@ -0,0 +1,220 @@
+[{
+  "_id": {
+    "$oid": "661d2813fe2e30afd4886858"
+  },
+  "username": "test3",
+  "email": "test3@test.com",
+  "password": "$2b$10$RPYPC0bGCGc5MIPLg0azX.PKFEBCweShEGfeErQ.EHIQar5AWqoMu",
+  "savedBooks": [],
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "661d292bfe2e30afd488685a"
+  },
+  "username": "test400",
+  "email": "test400@email.com",
+  "password": "$2b$10$C60OKywI7N/Lu1aXXwioCeUEnDjyCODsoQfRFWhmZOPB8a.HHnYfO",
+  "savedBooks": [
+    {
+      "authors": [
+        "Elizabeth Gilbert"
+      ],
+      "description": "The Number One international bestseller, Eat, Pray Love is a journey around the world, a quest for spiritual enlightenment and a story for anyone who has battled with divorce, depression and heartbreak.",
+      "bookId": "QdCNeNx6LJUC",
+      "image": "http://books.google.com/books/content?id=QdCNeNx6LJUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "Eat, Pray, Love",
+      "_id": {
+        "$oid": "661d2f54fe2e30afd488685c"
+      }
+    },
+    {
+      "authors": [
+        "Elizabeth Gilbert"
+      ],
+      "description": "A celebrated writer pens an irresistible, candid, and eloquent account of her pursuit of worldly pleasure, spiritual devotion, and what she really wanted out of life.",
+      "bookId": "purYCwAAQBAJ",
+      "image": "http://books.google.com/books/content?id=purYCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "Eat Pray Love",
+      "_id": {
+        "$oid": "661d2f5dfe2e30afd488685f"
+      }
+    },
+    {
+      "authors": [
+        "Pam Houston"
+      ],
+      "description": "“An absorbing, generous, ravishing book by a high priestess of you-have-to-read-this prose.\" —Cheryl Strayed, author of Wild Pam Houston, an \"early master of the art of rendering fiercely independent, brilliant women in love with the wrong men\" (Sarah Norris, Barnes & Noble Review), delivers a novel that whisks us from one breathtaking precipice to the next. Along the way, we unravel the story of Pam (a character not unlike the author), a fearless traveler aiming to leave her metaphorical baggage behind as she seeks a comfort zone in the air. With the help of a loyal cast of friends, body workers, and a new partner who helps her to be at home, she finally finds something like ground under her feet.",
+      "bookId": "bqirxyFVf7IC",
+      "image": "http://books.google.com/books/content?id=bqirxyFVf7IC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "Contents May Have Shifted: A Novel",
+      "_id": {
+        "$oid": "661d2f60fe2e30afd4886863"
+      }
+    }
+  ],
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "661d325d4c375c51d59e445a"
+  },
+  "username": "imbanu1",
+  "email": "imbanu1@gmail.com",
+  "password": "$2b$10$8bAmsCTGjKIm.mWrVFWkT.GvIh7EZ5dJLxXDRRW78mB28ODe3FQNm",
+  "savedBooks": [
+    {
+      "authors": [
+        "Stefan Bechtel"
+      ],
+      "description": "This collection of \"luck\" trivia provides the history of certain good luck rituals and objects, such as charms, knocking on wood, and wishbones, includes quotations about luck, and suggests ways to change one's luck from bad to good",
+      "bookId": "LIO1GNEkeiIC",
+      "image": "http://books.google.com/books/content?id=LIO1GNEkeiIC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+      "title": "The Good Luck Book",
+      "_id": {
+        "$oid": "661d32684c375c51d59e445c"
+      }
+    },
+    {
+      "authors": [
+        "No author to display"
+      ],
+      "bookId": "5XYPAAAAQAAJ",
+      "image": "http://books.google.com/books/content?id=5XYPAAAAQAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "The Guardian. ...",
+      "_id": {
+        "$oid": "661d45384c375c51d59e4461"
+      }
+    },
+    {
+      "authors": [
+        "Russell Hoban"
+      ],
+      "description": "‘Walker is my name and I am the same. Riddley Walker. Walking my riddels where ever theyve took me and walking them now on this paper the same. There aint that many sir prizes in life if you take noatis of every thing. Every time will have its happenings out and every place the same. Thats why I finely come to writing all this down. Thinking on what the idear of us myt be. Thinking on that thing whats in us lorn and loan and oansome.’ Composed in an English which has never been spoken and laced with a storytelling tradition that predates the written word, RIDDLEY WALKER is the world waiting for us at the bitter end of the nuclear road. It is desolate, dangerous and harrowing, and a modern masterpiece.",
+      "bookId": "2nvOJmqNqGMC",
+      "image": "http://books.google.com/books/content?id=2nvOJmqNqGMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "Riddley Walker",
+      "_id": {
+        "$oid": "661d453b4c375c51d59e4465"
+      }
+    },
+    {
+      "authors": [
+        "Luca Spaghetti"
+      ],
+      "description": "\"Luca Spaghetti is not only one of my favorite people in the world, but also a natural-born storyteller. . . . This [is a] marvelous book.\" -Elizabeth Gilbert When Luca Spaghetti (yes, that's really his name) was asked to show a writer named Elizabeth Gilbert around Rome, he had no idea how his life was about to change. She embraced his Roman ebullience, and Luca in turn became her guardian angel, determined that his city would help Liz out of her funk. Filled with colorful anecdotes about food, language, soccer, daily life in Rome, and Luca's own fish-out-of-water moments as a visitor to the United States-and culminating with the episodes in Liz's bestselling memoir, told from Luca's side of the table-Un Amico Italiano is a book that no fan of Eat, Pray, Love will want to miss.",
+      "bookId": "9TOMuCvA3BwC",
+      "image": "http://books.google.com/books/content?id=9TOMuCvA3BwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "Un Amico Italiano",
+      "_id": {
+        "$oid": "661d4d314c375c51d59e4476"
+      }
+    },
+    {
+      "authors": [
+        "No author to display"
+      ],
+      "description": "In the ten years since its electrifying debut, Elizabeth Gilbert's Eat Pray Love has become a worldwide phenomenon, empowering millions of readers to set out on paths they never thought possible. In this candid and captivating collection, nearly fifty of those readers – as diverse in their experiences as they are in age and background – share their stories. Eat Pray Love helped one woman to embrace motherhood, another to come to terms with the loss of her mother, and a third to find peace with not wanting to become a mother at all. One writer finds new love overseas; another embraces his sexual identity. The journeys they recount are transformative –sometimes hilarious, sometimes heartbreaking, but always inspiring. Entertaining and enlightening, Eat Pray Love Made Me Do It is a celebration for fans old and new.",
+      "bookId": "1wV-CwAAQBAJ",
+      "image": "http://books.google.com/books/content?id=1wV-CwAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+      "title": "Eat Pray Love Made Me Do It",
+      "_id": {
+        "$oid": "661d4d334c375c51d59e447c"
+      }
+    },
+    {
+      "authors": [
+        "Walter Pater"
+      ],
+      "bookId": "UtY7AAAAYAAJ",
+      "image": "http://books.google.com/books/content?id=UtY7AAAAYAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "Essays from The \"Guardian,\"",
+      "_id": {
+        "$oid": "661d4d83c3841aeaa0266402"
+      }
+    },
+    {
+      "authors": [
+        "Guardian Newspapers Ltd. Staff"
+      ],
+      "bookId": "2t3GHAAACAAJ",
+      "image": "",
+      "title": "Guardian Crossword Book Six",
+      "_id": {
+        "$oid": "661d4d86c3841aeaa026640a"
+      }
+    },
+    {
+      "authors": [
+        "Elizabeth Gilbert"
+      ],
+      "description": "For the first time the complete works of the award-winning author Elizabeth Gilbert are collected together, highlighting her talents as a writer of both fiction and non-fiction. In the international best-seller Eat, Pray, Love, Gilbert narrates her struggles after a bitter divorce and turbulent love affair, beginning her quest to rediscover how to be happy. In Rome, she indulges herself and gains nearly two stone. In India, she finds enlightenment through scrubbing temple floors. Finally, in Bali a toothless medicine man reveals a new path to peace, leaving her ready to find love again. In Committed, Gilbert is about to wed the man she fell in love with at the end of Eat, Pray, Love and with wit and intelligence contemplates marriage, trying with all her might to discover what this stubbornly enduring old institution actually is. In The Last American Man, Gilbert presents a fascinating, intimate portrait of the American naturalist and brilliant modern hero Eustace Conway, who at the age of seventeen ditched the comforts of his suburban existence to escape into the wild. Attempting to instil in people a deeper appreciation of nature, Conway stops at nothing in pursuit of bigger, bolder adventures. In Gilbert's first novel Stern Men, the eighteen-year-old irredeemably unromantic Ruth Thomas returns home from boarding school determined to join the 'stern-men'. Throwing her education overboard, this feisty and unforgettable American heroine helps work the lobster boats and brushes up on her profanity, eventually falling for a handsome young lobsterman. In Pilgrims, Gilbert's sharply drawn and tenderly observed collection of twelve short stories, tough heroes and heroines, hardened by their experiences, struggle for their epiphanies and seek companionship as fiercely as they can.",
+      "bookId": "iISPq7MYxoMC",
+      "image": "http://books.google.com/books/content?id=iISPq7MYxoMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "The Complete Elizabeth Gilbert",
+      "_id": {
+        "$oid": "661d9b3dc3841aeaa026644b"
+      }
+    },
+    {
+      "authors": [
+        "Kristan Higgins"
+      ],
+      "description": "'Good Luck With That is a lesson in kindness, acceptance, and self-worth. I'm so glad I read this book, and I hope every woman gives herself this gift' Reader review ---- Emerson, Georgia, and Marley have been best friends ever since they met at a weight-loss camp as teens. When Emerson tragically passes away, she leaves one final wish for her best friends: to conquer the fears they still carry as adults. For each of them, that means something different. For Marley, it's coming to terms with the survivor's guilt she's carried around since her twin sister's death, which has left her blind to the real chance for romance in her life. For Georgia, it's about learning to stop trying to live up to her mother's and brother's ridiculous standards, and learning to accept the love her ex-husband has tried to give her. But as Marley and Georgia grow stronger, the real meaning of Emerson's dying wish becomes truly clear: more than anything, she wanted her friends to love themselves. A novel of compassion and insight, Good Luck With That tells the story of two women who learn to embrace themselves just the way they are. ---- Real readers have fallen in love with Good Luck with That 'A must read from the very talented Kristan Higgins, who just gets better and better with each book she writes' 5 star reader review 'What a book! Tears were flowing at the end' 5 star reader review 'If ever there was a book that demands to be discussed, it's this one. I wish I were part of a book club because Good Luck with That is the one I'd demand everyone read' 5 star reader review 'Wonderful story. I would highly recommend' 5 star reader review 'The people in this book caught hold of my heart and interest from the first few pages. Finished in three evenings and was sorry when it came to an end. Many a tear both sad and happy' 5 star reader review",
+      "bookId": "xgSxEAAAQBAJ",
+      "image": "http://books.google.com/books/content?id=xgSxEAAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+      "title": "Good Luck with That",
+      "_id": {
+        "$oid": "661d9b79ae7e58210388c331"
+      }
+    },
+    {
+      "authors": [
+        "Chris Oyakhilome"
+      ],
+      "bookId": "jv1C5ywqSwEC",
+      "image": "http://books.google.com/books/content?id=jv1C5ywqSwEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "How to Make Your Faith Work!",
+      "_id": {
+        "$oid": "661da05eae7e58210388c350"
+      }
+    },
+    {
+      "authors": [
+        "Stefan Bechtel"
+      ],
+      "description": "This collection of \"luck\" trivia provides the history of certain good luck rituals and objects, such as charms, knocking on wood, and wishbones, includes quotations about luck, and suggests ways to change one's luck from bad to good",
+      "bookId": "LIO1GNEkeiIC",
+      "image": "http://books.google.com/books/content?id=LIO1GNEkeiIC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+      "title": "The Good Luck Book",
+      "_id": {
+        "$oid": "6628040b834f45f408e04ed8"
+      }
+    },
+    {
+      "authors": [
+        "Martin Meadows"
+      ],
+      "description": "How to Build Self-Discipline and Become More Successful (365 Powerful Thoughts From the World’s Brightest Minds) Its lack makes you unable to achieve your goals. Without it, you’ll struggle to lose weight, become fit, wake up early, work productively and save money. Not embracing it in your everyday life means that you’ll never realize your full potential. Ignoring it inevitably leads to regret and feeling sad about how more successful and incredible your life could have been if you had only decided to develop it. What is this powerful thing? Self-discipline. And if there’s one thing that self-discipline is not, it’s instant. It takes months (if not years) to develop powerful self-control that will protect you from impulsive decisions, laziness, procrastination, and inaction. You need to exhibit self-discipline day in, day out, 365 days in a year. What if you had a companion who would remind you daily to stay disciplined and persevere, even when the going gets tough? 365 Days With Self-Discipline is a practical, accessible guidebook for embracing more self-discipline in your everyday life. You’ll learn how to do this through 365 brief, daily insights from the world’s brightest minds, expanded and commented upon by bestselling personal development author Martin Meadows. This isn’t just an inspirational book; most of the entries deliver practical suggestions that you can immediately apply in your life to become more disciplined. Here are just some of the things you’ll learn: - why living your life the hard way makes it easy (and other suggestions from a successful entrepreneur and longevity scientist); - how to overcome your initial resistance and procrastination based on the remark made by one of the most renowned Renaissance men; - why, according to an influential neurosurgeon, it’s key to see problems as hurdles instead of obstacles (and how to do that); - how to embrace an experimental mindset to overcome a fear of failure (a technique recommended by a successful entrepreneur and musician); - how to quit in a smart way, according to a world-famous marketing expert; - how to improve your productivity at work by implementing the advice from one of the most successful detective fiction writers; - how a trick used by screenwriters can help you figure out the first step needed to get closer to your goals; - how to maintain self-discipline in the long-term by paying attention to what a bestselling non-fiction author calls necessary to survive and thrive; - how your most common thoughts can sabotage your efforts (and other valuable insights from one of the most respected Roman Stoics); and - how to overcome temporary discouragement and look at your problems from the proper perspective, as suggested by a well-known public speaker and author. If you’re ready to finally change your life and embrace self-discipline — not only for the next 365 days, but for the rest of your life — buy this book now and together, let’s work on your success! Keywords: self-discipline handbook, self-control book, willpower book, success journal, mental resilience, become successful, achieve your goals",
+      "bookId": "lMBEDwAAQBAJ",
+      "image": "http://books.google.com/books/content?id=lMBEDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "365 Days With Self-Discipline",
+      "_id": {
+        "$oid": "662851611215130dbc6d95a5"
+      }
+    },
+    {
+      "authors": [
+        "Tony T. Kern"
+      ],
+      "description": "Flight Discipline is the complete tool kit for any aviator, whether military, commercial, or recreational, to develop the crack discipline needed to be a safe and effective aviator. Major Tony Kern analyses the causes of poor flight discipline, gives chilling case studies of the consequences, and lays out a plan for individual improvement. Key words are italicized and review questions included for each chapter. An unequalled guide to this mainspring of good piloting.",
+      "bookId": "VzgibwtOleAC",
+      "image": "http://books.google.com/books/content?id=VzgibwtOleAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      "title": "Flight Discipline (PB)",
+      "_id": {
+        "$oid": "662851631215130dbc6d95b3"
+      }
+    }
+  ],
+  "__v": 0
+}]
